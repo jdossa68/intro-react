90e1c68f06f7357eee841dce3f547b172ccfa967
Juan Diego <ossajuandiego@gmail.com>
2022-03-01 20:47:49 -0500

    Updates

---
+++ precache-manifest.b10692169e9c47c054d4a7f798662ed2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "99ae5bdf8d62e4ddcfb8",
+    "url": "/intro-react/static/css/main.66b0791b.chunk.css"
+  },
+  {
+    "revision": "99ae5bdf8d62e4ddcfb8",
+    "url": "/intro-react/static/js/main.99ae5bdf.chunk.js"
+  },
+  {
+    "revision": "ceefb4fbb137ac8ec744",
+    "url": "/intro-react/static/js/1.ceefb4fb.chunk.js"
+  },
+  {
+    "revision": "f949c68cdf70264507ba",
+    "url": "/intro-react/static/js/runtime~main.f949c68c.js"
+  },
+  {
+    "revision": "39504ce52753d6c6047b663a8a1c29b2",
+    "url": "/intro-react/static/media/check.39504ce5.svg"
+  },
+  {
+    "revision": "375d6728e52a83680aeae436f7385f4f",
+    "url": "/intro-react/static/media/delete.375d6728.svg"
+  },
+  {
+    "revision": "03e3c6a96e85eec5afde2f33315f8517",
+    "url": "/intro-react/index.html"
+  }
+];
